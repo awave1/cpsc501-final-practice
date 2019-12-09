42cb8aed6f463ef26b9099894b2803ad6807f7da
Artem Golovin <artgolovin96@gmail.com>
2019-12-09 10:06:34 -0700

    :sparkles: add some dumb questions to do

--- src/main/java/reflection/Main.java
+++ src/main/java/reflection/Main.java
@@ -1,5 +1,31 @@
 package reflection;
 
+/**
+ * Do the following using reflection (inside the `main`)
+ * Q1:
+ *
+ * 1. Create `Person` object
+ * 2. Call private `setPerson` and set values to whatever
+ * 3. "Serialize" (print to stdout) xml representation of the object. Format:
+ *
+ *                          id needed if there's a reference. (id can be stored in the value)
+ *                          same as in assignment
+ *      <object name="name" id="0">
+ *        <field name="name">
+ *            value
+ *        </field>
+ *      </object>
+ *
+ *
+ * Q2:
+ *
+ * 1. Create `Car` object
+ * 2. call `setName` and set name to whatever
+ * 3. set driver to the driver created in prev. question
+ * 4. serialize it the same way as q1
+ */
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("uhhhh");
+    }
 }
